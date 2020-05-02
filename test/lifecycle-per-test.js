dd6841ef208764c992f9444b6ea4d61ab02d9d74
Alex Feldman <afeldman@paypal.com>
2020-05-01 21:56:52 -0400

    fixed tests for global beforeEach hook

--- test/lifecycle-per-test.js
+++ test/lifecycle-per-test.js
@@ -1,38 +1,3 @@
-describe('Nemo Instance for @entireSuite@', function () {
-  before(function () {
-    if (this.nemo) {
-      return Promise.resolve();
-    }
-    return Promise.reject(new Error('no nemo'));
-  });
-  after(function () {
-    if (this.nemo) {
-      return Promise.resolve();
-    }
-    return Promise.reject(new Error('no nemo'));
-  });
-  beforeEach(function () {
-    if (this.nemo) {
-      return Promise.resolve();
-    }
-    return Promise.reject(new Error('no nemo'));
-  });
-  afterEach(function () {
-    if (this.nemo) {
-      return Promise.resolve();
-    }
-    return Promise.reject(new Error('no nemo'));
-  });
-  it('should pass', function () {
-    let nemo = this.nemo;
-    //verify nemo.mocha property
-    if (!nemo.mocha === this) {
-      return Promise.reject(new Error('didnt find mocha context at nemo.mocha'));
-    }
-    return nemo.driver.get(nemo.data.baseUrl);
-  });
-});
-
 beforeEach('Nemo Instance for global beforeEach @eachTest@', function () {
   if (this.nemo) {
     return Promise.resolve();
