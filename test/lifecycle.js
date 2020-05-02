9de210aa30ab83679bdf5dd6bba278069b986015
Alex Feldman <afeldman@paypal.com>
2020-05-01 21:48:18 -0400

    added global beforeEach nemo injection

--- test/lifecycle.js
+++ test/lifecycle.js
@@ -1,4 +1,3 @@
-
 describe('Nemo Instance for @entireSuite@', function () {
   before(function () {
     if (this.nemo) {
@@ -32,11 +31,24 @@ describe('Nemo Instance for @entireSuite@', function () {
     }
     return nemo.driver.get(nemo.data.baseUrl);
   });
+});
 
+beforeEach('Nemo Instance for global beforeEach @eachTest@', function () {
+  if (this.nemo) {
+    return Promise.resolve();
+  }
+  return Promise.reject(new Error('no nemo'));
+});
+
+afterEach('Nemo Instance for global afterEach @eachTest@', function () {
+  if (this.nemo) {
+    return Promise.resolve();
+  }
+  return Promise.reject(new Error('no nemo'));
 });
 
 describe('Nemo Instance for @eachTest@', function () {
-  beforeEach(function () {
+   beforeEach(function () {
     if (this.nemo) {
       return Promise.resolve();
     }
@@ -56,5 +68,26 @@ describe('Nemo Instance for @eachTest@', function () {
     }
     return nemo.driver.get(nemo.data.baseUrl);
   });
-
+  describe('Nemo Nested Instance for @eachTest@', function () {
+    beforeEach(function () {
+      if (this.nemo) {
+        return Promise.resolve();
+      }
+      return Promise.reject(new Error('no nemo'));
+    });
+    afterEach(function () {
+      if (this.nemo) {
+        return Promise.resolve();
+      }
+      return Promise.reject(new Error('no nemo'));
+    });
+    it('should pass', function () {
+      let nemo = this.nemo;
+      //verify nemo.mocha property
+      if (!nemo.mocha === this) {
+        return Promise.reject(new Error('didnt find mocha context at nemo.mocha'));
+      }
+      return nemo.driver.get(nemo.data.baseUrl);
+    });
+  });
 });
